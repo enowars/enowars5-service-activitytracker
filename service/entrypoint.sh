6d08bbe66d2a8fc234aa9bcd8177868787fd186d
Richard Viehöver <richard.viehoever@gmail.com>
2021-05-13 19:48:51 +0200

    Fix Dockerfile and update github actions runner

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -4,12 +4,17 @@ set -x
 
 mkdir -p /data/imgs
 
+# wait for database connection
 until nc -vz -w 100 postgres 5432
 do
   sleep 1
 done
 
-diesel setup
+# wait until db system is started up
+until diesel setup
+do
+  sleep 1
+done
 diesel migration run
 
 cargo run --release
